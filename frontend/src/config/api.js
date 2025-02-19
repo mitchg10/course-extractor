c4a68fd9bde5f51a5299caf913177c62eb4f5e73
mitchg10 <mitchg@vt.edu>
2025-02-19 13:33:35 -0500

    edit base url

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,17 +1,6 @@
-// export const API_URL = process.env.NODE_ENV === 'production' 
-//   ? 'https://course-extractor-rior.onrender.com'
-//   : 'http://localhost:8000';
-
-// export const endpoints = {
-//     process: `${API_URL}/process`,
-//     status: (taskId) => `${API_URL}/status/${taskId}`,
-//     availableFiles: (taskId) => `${API_URL}/available-files/${taskId}`,
-//     download: (taskId, filename) => `${API_URL}/download/${taskId}/${filename}`
-// };
-
 const BASE_URL = process.env.NODE_ENV === 'development' 
   ? 'http://localhost:8000'
-  : '';  // Use relative paths in production
+  : 'https://course-extractor-rior.onrender.com';
 
 export const endpoints = {
     process: `${BASE_URL}/process`,
